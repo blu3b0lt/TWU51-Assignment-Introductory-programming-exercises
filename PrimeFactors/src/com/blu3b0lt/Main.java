b41c2e9f9659ce958e23974ed71c1b3fbe44bb24
Nithish <nithishsankar@outlook.com>
2016-06-24 11:42:23 +0530

    Skeleton for PrimeFactors

---
+++ PrimeFactors/src/com/blu3b0lt/Main.java
@@ -0,0 +1,8 @@
+package com.blu3b0lt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ PrimeFactors/src/org/blu3b0lt/PrimeFactor.java
@@ -0,0 +1,7 @@
+package org.blu3b0lt;
+
+/**
+ * Created by blu3b0lt on 24/6/16.
+ */
+public class PrimeFactor {
+}
