6f2679ef6fc2e8917497e00eb4812a839e7d1bbb
Nithish <nithishsankar@outlook.com>
2016-06-24 10:16:15 +0530

    Completed Triangle Exercises

--- TriangleExercises/src/com/company/Main.java
+++ TriangleExercises/src/com/company/Main.java
@@ -1,8 +1,108 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private String printString;
+
+    Main() {
+        printString = "*";
+    }
+
+    Main(String aPrintString) {
+        printString = aPrintString;
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        Main exerciseStub = new Main("*");
+        while(true) {
+            exerciseStub.showMenu();
+            Scanner in = new Scanner(System.in);
+            int option = exerciseStub.captureInput();
+            switch(option) {
+                case 1 : {
+                    System.out.println("Output is: ");
+                    exerciseStub.easiestExerciseEver();
+                    break;
+                }
+                case 2:{
+                    System.out.println("Output is: ");
+                    exerciseStub.drawHorizontalLine();
+                    break;
+                }
+                case 3: {
+                    System.out.println("Output is: ");
+                    exerciseStub.drawVerticalLine();
+                    break;
+                }
+                case 4: {
+                    System.out.println("Output is: ");
+                    exerciseStub.drawRightTriangle();
+                    break;
+                }
+
+                case 5: {
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    public void showMenu() {
+        System.out.println("Welcome to Triangle Generator");
+        System.out.println("Menu:\n1.Easiest Exercise Ever\n2.Draw Horizontal Line\n3.Draw Vertical Line\n4.Draw Right Triangle");
+        System.out.println("5.Exit");
+        System.out.print("Enter your choice: ");
+        return;
+    }
+
+    public void easiestExerciseEver() {
+        System.out.println(printString);
+        return;
+    }
+
+    public void drawRightTriangle() {
+        System.out.print("Enter 'n': ");
+        int lines = captureInput();
+        int times = 1;
+        while(lines > 0) {
+            String output = "";
+            for(int counter = 0; counter < times; counter++) {
+                output += printString;
+            }
+            lines--;
+            times++;
+            System.out.println(output);
+        }
+        return;
+    }
+
+    public void drawHorizontalLine() {
+        System.out.print("Enter 'n': ");
+        int lines = captureInput();
+        String output = "";
+        while(lines > 0) {
+            output += printString;
+            lines--;
+        }
+        System.out.println(output);
+        return;
+    }
+
+    public void drawVerticalLine() {
+        System.out.print("Enter 'n': ");
+        int lines = captureInput();
+        while(lines > 0) {
+            System.out.println(printString);
+            lines--;
+        }
+        return;
+    }
+
+    public int captureInput() {
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
     }
 }
